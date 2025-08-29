338896c116dc48d393fa0222beffdd18f6a854dd
Tim Usner <tim.usner@sap.com>
2025-08-29 12:16:34 +0200

    Add generation for `Extension` example (#16)

--- example/doc.go
+++ example/doc.go
@@ -6,6 +6,8 @@
 //go:generate sh -c "find . -iname '20*' | grep -Ev '_clusters|_extensions|_managedresource' | xargs rm"
 //go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh extension-image-rewriter ../charts/gardener-extension-image-rewriter $(cat ../VERSION) controller-registration.yaml Extension:image-rewriter"
 //go:generate sh -c "kustomize build -o controller-registration.yaml"
+//go:generate sh -c "extension-generator --name=extension-image-rewriter --provider-type=image-rewriter --component-category=extension --extension-oci-repository=europe-docker.pkg.dev/gardener-project/public/charts/gardener/extensions/image-rewriter:$(cat ../VERSION) --destination=./extension/base/extension.yaml"
+//go:generate sh -c "$TOOLS_BIN_DIR/kustomize build ./extension -o ./extension.yaml"
 
 // Package example contains generated manifests for all CRDs and other examples.
 // Useful for development purposes.
